0e288a5d774c79504b18c5f0308d2f0a24517116
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-12-24 21:22:03 +0300

    feat(CountTypesList): create component

---
+++ components/CountTypesList.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+
+const COUNT_TYPES = [
+  { label: "Hour", value: "hour" },
+  { label: "Day", value: "day" },
+  { label: "Week", value: "week" },
+  { label: "Month", value: "month" },
+  { label: "Year", value: "year" },
+];
+
+const CountTypesList = () => {
+  const [type, setType] = useState(COUNT_TYPES[1].value);
+  return (
+    <Picker
+      selectedValue={type}
+      style={{ height: 50 }}
+      onValueChange={(item) => setType(item.toString())}
+      mode="dropdown"
+    >
+      {COUNT_TYPES.map((typeItem) => (
+        <Picker.Item key={typeItem.value} label={typeItem.label} value={typeItem.value}/>
+      ))}
+    </Picker>
+  );
+};
+
+export default CountTypesList;
