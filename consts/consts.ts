2c4129e35b61cbf847d3e389e2e6678ae375455e
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-12-29 21:35:48 +0300

    chore(consts): add ColorScheme

---
+++ consts/consts.ts
@@ -0,0 +1,11 @@
+const ColorScheme = {
+  DARK_BLUE_MAIN: "#12232E",
+  DARK_BLUE_SUB: "#203647",
+  LIGHT_WHITE: "#EEFBFB",
+  LIGHTER_BLUE: "#007CC7",
+  LIGHTEST_BLUE: "#4DA8DA",
+};
+
+export {
+  ColorScheme,
+};
