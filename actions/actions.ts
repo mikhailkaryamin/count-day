4555f2dad4f52c2ad90a60676e9d6fe32693c071
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-12-30 23:36:31 +0300

    feat(actions): add setTitle, setDate, setCountFor, setCountOnlySelectionDay

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,10 +1,26 @@
-import { Types, ModalActions, EventActions } from "../reducers/reducers";
+import { Types, ModalActions, EventActions, CountFor } from "../shared/types";
 
 const ActionCreator = {
-  showModal: (payload): ModalActions | EventActions => ({
+  showModal: (payload: boolean): ModalActions | EventActions => ({
     type: Types.ShowModal,
     payload,
-  })
+  }),
+  setTitle: (payload: string): ModalActions | EventActions => ({
+    type: Types.SetTitle,
+    payload,
+  }),
+  setDate: (payload: Date): ModalActions | EventActions => ({
+    type: Types.SetDate,
+    payload,
+  }),
+  setCountFor: (payload: CountFor): ModalActions | EventActions => ({
+    type: Types.SetCountFor,
+    payload,
+  }),
+  setCountOnlySelectionDay: (payload: string[]): ModalActions | EventActions => ({
+    type: Types.SetCountOnlySelectionDay,
+    payload,
+  }),
 };
 
 
