772e12825dc082002d6325c0389ea233f90ee9f9
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-12-30 23:39:54 +0300

    chore(reducers): transfer types to types.ts

--- reducers/reducers.ts
+++ reducers/reducers.ts
@@ -1,23 +1,4 @@
-type ActionMap<M extends { [index: string]: any }> = {
-  [Key in keyof M]: M[Key] extends undefined
-    ? {
-        type: Key;
-      }
-    : {
-        type: Key;
-        payload: M[Key];
-      }
-};
-
-export enum Types {
-  ShowModal = "SHOW_MODAL",
-  SetCurrentEventId = "SET_CURRENT_EVENT_ID",
-}
-
-type ModalPayload = {
-  [Types.ShowModal]: boolean;
-}
-export type ModalActions = ActionMap<ModalPayload>[keyof ActionMap<ModalPayload>];
+import { EventType, Types, ModalActions, EventActions } from "../shared/types";
 
 const modalReducer = (state: boolean, action: ModalActions | EventActions) => {
   switch (action.type) {
@@ -28,24 +9,33 @@ const modalReducer = (state: boolean, action: ModalActions | EventActions) => {
   }
 };
 
-type EventType = {
-  id: number;
-};
-
-type EventPayload = {
-  [Types.SetCurrentEventId] : {
-    id: number;
-  }
-}
-
-export type EventActions = ActionMap<EventPayload>[keyof ActionMap<EventPayload>];
 
 const eventReducer = (state: EventType | null, action: ModalActions | EventActions) => {
   switch (action.type) {
     case Types.SetCurrentEventId:
       return {
         ...state,
-        id: action.payload.id,
+        id: action.payload,
+      };
+    case Types.SetDate:
+      return {
+        ...state,
+        date: action.payload,
+      };
+    case Types.SetTitle:
+      return {
+        ...state,
+        title: action.payload,
+      };
+    case Types.SetCountFor:
+      return {
+        ...state,
+        countFor: action.payload,
+      };
+    case Types.SetCountOnlySelectionDay:
+      return {
+        ...state,
+        countOnlySelectionDay: action.payload,
       };
     default:
       return state;
