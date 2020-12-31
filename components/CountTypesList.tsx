22cd97e502159781ec44d8a71b2120973cff0ec9
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-12-31 15:27:26 +0300

    feat(CountTypesList): add feat write type to context

--- components/CountTypesList.tsx
+++ components/CountTypesList.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Picker } from "@react-native-picker/picker";
 
+import { ActionCreator } from "../actions/actions";
+
+import { AppContext } from "../shared/context";
 import { ColorScheme } from "../shared/consts";
+import { CountFor } from "../shared/types";
 
 const COUNT_TYPES = [
   { label: "Hour", value: "hour" },
@@ -9,15 +13,26 @@ const COUNT_TYPES = [
   { label: "Week", value: "week" },
   { label: "Month", value: "month" },
   { label: "Year", value: "year" },
-];
+] as const;
 
 const CountTypesList = () => {
-  const [type, setType] = useState(COUNT_TYPES[1].value);
+  const [type, setType] = useState<CountFor>(COUNT_TYPES[1].value);
+
+  const { dispatch } = useContext(AppContext);
+
+  const onChange = (item: CountFor) => {
+    setType(item);
+  };
+
+  useEffect(() => {
+    dispatch(ActionCreator.setCountFor(type));
+  }, [type]);
+
   return (
     <Picker
       selectedValue={type}
       style={{ height: 50, color: ColorScheme.LIGHT_WHITE }}
-      onValueChange={(item) => setType(item.toString())}
+      onValueChange={onChange}
       mode="dropdown"
     >
       {COUNT_TYPES.map((typeItem) => (
