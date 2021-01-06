1483fa4fc2fd59b255654e2c1233be87cef32cc4
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-06 22:02:58 +0300

    feat(ButtonAdd): create componetn to do add event

---
+++ components/ButtonAdd.tsx
@@ -0,0 +1,52 @@
+import React, { useContext } from "react";
+import { View, StyleSheet, Pressable, Text } from "react-native";
+
+import { AppContext } from "../shared/context";
+import { ActionsTypes } from "../shared/types";
+import { ColorScheme } from "../shared/consts";
+
+import EventItemContainer from "./EventItemContainer";
+
+const ButtonAdd = () => {
+  const { dispatch } = useContext(AppContext);
+
+  return (
+
+    <EventItemContainer bgColor={ColorScheme.LIGHTER_BLUE}>
+      <View style={styles.container}>
+        <Pressable
+          onPress={() =>
+            dispatch({
+              type: ActionsTypes.ShowModal,
+              payload: true,
+            })
+          }
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Add new event</Text>
+          </View>
+        </Pressable>
+      </View>
+    </EventItemContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    borderWidth: 3,
+    borderRadius: 10,
+    borderColor: ColorScheme.LIGHTEST_BLUE,
+    backgroundColor: ColorScheme.LIGHTEST_BLUE,
+  },
+  button: {
+    alignItems: "center",
+    padding: 10,
+  },
+  buttonText: {
+    color: ColorScheme.LIGHT_WHITE,
+    fontSize: 24,
+  }
+});
+
+export default ButtonAdd;
