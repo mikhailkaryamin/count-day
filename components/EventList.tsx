ae8884079cf04a256e595e5ca64fb375a4c31614
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-06 22:11:04 +0300

    chore(EvetnList): highlighted container for event item

--- components/EventList.tsx
+++ components/EventList.tsx
@@ -1,11 +1,10 @@
-import React, { useContext } from "react";
-import { View, FlatList, StyleSheet, Text, Pressable } from "react-native";
+import React from "react";
+import { View, FlatList, StyleSheet, Text } from "react-native";
 
-
-import { Types } from "../shared/types";
-import { AppContext } from "../shared/context";
 import { ColorScheme } from "../shared/consts";
 import { ItemType, RenderItemType } from "../shared/types";
+
+import EventItemContainer from "./EventItemContainer";
 
 const MOCK_DATA = [
   {
@@ -31,30 +30,21 @@ const MOCK_DATA = [
 ];
 
 const Item = ({ title, date }: ItemType) => {
-  const { dispatch } = useContext(AppContext);
-
   return (
-    <View style={styles.item}>
-      <Pressable
-        onPress={() =>
-          dispatch({
-            type: Types.ShowModal,
-            payload: true,
-          })
-        }
-      >
-        <View style={styles.itemContainer}>
-          <View style={styles.itemLeft}>
-            <Text style={styles.title}>{title}</Text>
-            <Text style={styles.title}>{date}</Text>
-          </View>
-          <View style={styles.itemRight}>
-            <Text style={{ ...styles.title, ...styles.countNumber }}>25</Text>
-            <Text style={{ ...styles.title, ...styles.countType }}>d</Text>
-          </View>
+    <EventItemContainer
+      bgColor={ColorScheme.DARK_BLUE_MAIN}
+    >
+      <View style={styles.itemContainer}>
+        <View style={styles.itemLeft}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.title}>{date}</Text>
         </View>
-      </Pressable>
-    </View>
+        <View style={styles.itemRight}>
+          <Text style={{ ...styles.title, ...styles.countNumber }}>25</Text>
+          <Text style={{ ...styles.title, ...styles.countType }}>d</Text>
+        </View>
+      </View>
+    </EventItemContainer>
   );
 };
 
@@ -78,12 +68,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
-  },
-  item: {
-    backgroundColor: ColorScheme.DARK_BLUE_MAIN,
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
   },
   itemContainer: {
     flex: 1,
