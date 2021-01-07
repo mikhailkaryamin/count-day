b4e3e915b503bc5a58249a286bf093db4edec090
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-07 18:57:38 +0300

    chore(consts): add KeyStorage

--- shared/consts.ts
+++ shared/consts.ts
@@ -4,14 +4,19 @@ const ColorScheme = {
   LIGHT_WHITE: "#EEFBFB",
   LIGHTER_BLUE: "#007CC7",
   LIGHTEST_BLUE: "#4DA8DA",
-};
+} as const;
 
 const StatusStorage = {
   ERROR: "error",
   SUCCESS: "success",
 } as const;
 
+const KeyStorage = {
+  EVENT: "@events_storage"
+};
+
 export {
   ColorScheme,
+  KeyStorage,
   StatusStorage,
 };
