dd2184e396bff2048de136d1e59bc87e74a4ead5
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-08 23:16:50 +0300

    feat(EventsList): add getFormatEventsList, in Item add countDate from prop

--- components/EventsList.tsx
+++ components/EventsList.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { View, FlatList, StyleSheet, Text } from "react-native";
-import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 
-import { ColorScheme, KeyStorage } from "../shared/consts";
+import { AppContext } from "../shared/context";
+import { ColorScheme } from "../shared/consts";
 import { ItemType, RenderItemType } from "../shared/types";
 
 import EventItemContainer from "./EventItemContainer";
 
-const Item = ({ title, date }: ItemType) => {
+const Item = ({ title, date, countDate }: ItemType) => {
   return (
     <EventItemContainer
       bgColor={ColorScheme.DARK_BLUE_MAIN}
@@ -18,7 +18,7 @@ const Item = ({ title, date }: ItemType) => {
           <Text style={styles.title}>{date}</Text>
         </View>
         <View style={styles.itemRight}>
-          <Text style={{ ...styles.title, ...styles.countNumber }}>25</Text>
+          <Text style={{ ...styles.title, ...styles.countNumber }}>{countDate}</Text>
           <Text style={{ ...styles.title, ...styles.countType }}>d</Text>
         </View>
       </View>
@@ -27,27 +27,34 @@ const Item = ({ title, date }: ItemType) => {
 };
 
 const EventsList = () => {
-  const [value, setValue] = useState(null);
-  const { getItem } = useAsyncStorage(KeyStorage.EVENT);
+  const { state } = useContext(AppContext);
+  console.log("EventsList", state.events, state);
+  const getFormatEventsList = () => {
+    if (state.events?.length) {
+      const formatEvents = state.events.map((item) => {
+        const itemDate = new Date(item.date);
+        return ({
+          id: item.id,
+          title: item.title,
+          date: itemDate.toLocaleDateString(),
+          countDate: "25",
+        });
+      });
 
-  const readItemFromStorage = async () => {
-    const item = await getItem();
-    setValue(item);
+      return formatEvents;
+    } else {
+      return [];
+    }
   };
 
-  useEffect(() => {
-    readItemFromStorage();
-  }, []);
-
   const renderItem = ({ item }: RenderItemType) => (
-    <Item title={item.title} date={item.date} />
+    <Item title={item.title} date={item.date} countDate={item.countDate} />
   );
-
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={[]}
+        data={getFormatEventsList()}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
