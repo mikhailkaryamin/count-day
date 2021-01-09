c6d6a05997dcf6b4d2fc7487f032a24efde324a0
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-09 23:42:47 +0300

    chore(consts): add CountTypes, MS

--- shared/consts.ts
+++ shared/consts.ts
@@ -1,3 +1,10 @@
+const CountTypes = {
+  Day: "day",
+  Week: "week",
+  Month: "month",
+  Year: "year",
+} as const;
+
 const ColorScheme = {
   DARK_BLUE_MAIN: "#12232E",
   DARK_BLUE_SUB: "#203647",
@@ -6,17 +13,27 @@ const ColorScheme = {
   LIGHTEST_BLUE: "#4DA8DA",
 } as const;
 
+const KeyStorage = {
+  EVENT: "@events_storage"
+};
+
+const MS = {
+  YEAR: 31556952000,
+  MONTH: 2629800000,
+  WEEK: 604800000,
+  DAY: 86400000,
+};
+
 const StatusStorage = {
   ERROR: "error",
   SUCCESS: "success",
 } as const;
 
-const KeyStorage = {
-  EVENT: "@events_storage"
-};
 
 export {
+  CountTypes,
   ColorScheme,
   KeyStorage,
+  MS,
   StatusStorage,
 };
