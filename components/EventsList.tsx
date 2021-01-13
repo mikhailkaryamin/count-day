a2eaafe061af87f7b263282a86a51655c3cb5017
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-13 21:14:46 +0300

    feat(EventsList): add prefix passed or left to date

--- components/EventsList.tsx
+++ components/EventsList.tsx
@@ -3,28 +3,54 @@ import { View, FlatList, StyleSheet, Text, Pressable } from "react-native";
 
 import { ActionCreator } from "../actions/actions";
 import { AppContext } from "../shared/context";
-import { ColorScheme } from "../shared/consts";
-import { ItemType, RenderItemType } from "../shared/types";
-import { calculateOffset } from "../shared/utils";
+import { ColorScheme, CountTypes } from "../shared/consts";
+import { ItemType, RenderItemType, CountOnlySelectedDays, CountFor } from "../shared/types";
+import { calculateOffset, calculateOffsetSelectedDays } from "../shared/utils";
 
 import EventItemContainer from "./EventItemContainer";
 
-const EventItem = ({ title, date, countDate, countType, onPressItem }: ItemType) => {
+const getOffsetCount = (date: string, countFor: CountFor, selectedDays: CountOnlySelectedDays) => {
+  if (countFor === CountTypes.Day) {
+    const isSelectedAllDays = selectedDays.map((dayItem) => dayItem[1]).every((day) => day);
+
+    return (
+      isSelectedAllDays ? calculateOffset(date, countFor) : calculateOffsetSelectedDays(date, selectedDays)
+    );
+  } else {
+    return calculateOffset(date, countFor);
+  }
+};
+
+const EventItem = ({
+  title,
+  date,
+  countDate,
+  countType,
+  onPressItem,
+  prefixOffsetCount,
+}: ItemType) => {
   return (
-    <EventItemContainer
-      bgColor={ColorScheme.DARK_BLUE_MAIN}
-    >
-      <Pressable
-        onPress={onPressItem}
-      >
+    <EventItemContainer bgColor={ColorScheme.DARK_BLUE_MAIN}>
+      <Pressable onPress={onPressItem}>
         <View style={styles.itemContainer}>
           <View style={styles.itemLeft}>
             <Text style={styles.title}>{title}</Text>
             <Text style={styles.title}>{date}</Text>
           </View>
           <View style={styles.itemRight}>
-            <Text style={{ ...styles.title, ...styles.countNumber }}>{countDate}</Text>
-            <Text style={{ ...styles.title, ...styles.countType }}>{countType}</Text>
+            <View style={styles.itemRightTop}>
+              <Text style={{ ...styles.title }}>
+                {prefixOffsetCount}
+              </Text>
+            </View>
+            <View style={styles.itemRightBottom}>
+              <Text style={{ ...styles.title, ...styles.countNumber }}>
+                {countDate}
+              </Text>
+              <Text style={{ ...styles.title, ...styles.countType }}>
+                {countType}
+              </Text>
+            </View>
           </View>
         </View>
       </Pressable>
@@ -39,16 +65,19 @@ const EventsList = () => {
     if (state.events?.length) {
       const formatEvents = state.events.map((item) => {
         const dateEvent = new Date(item.date).toLocaleDateString();
-        const offsetCount = calculateOffset(item.date, item.countFor);
+        const offsetCount = getOffsetCount(item.date, item.countFor, item.countOnlySelectionDay);
         const countTypeChar = item.countFor.charAt(0);
+        const offsetCountStr = offsetCount < 0 ? offsetCount.toString().slice(1) : offsetCount.toString();
+        const prefixOffsetCount = offsetCount > 0 ? "passed" : "left";
 
-        return ({
+        return {
           id: item.id,
           title: item.title,
           date: dateEvent,
-          countDate: offsetCount,
+          countDate: offsetCountStr,
           countType: countTypeChar,
-        });
+          prefixOffsetCount,
+        };
       });
 
       return formatEvents;
@@ -58,14 +87,23 @@ const EventsList = () => {
   };
 
   const onPressItem = (id: string) => {
-    const currentEvent = state.events.filter((eventItem) => eventItem.id === id);
+    const currentEvent = state.events.filter(
+        (eventItem) => eventItem.id === id
+    );
 
     dispatch(ActionCreator.setCurrentEvent(currentEvent[0]));
     dispatch(ActionCreator.showModal(true));
   };
 
   const renderItem = ({ item }: RenderItemType) => (
-    <EventItem title={item.title} date={item.date} countDate={item.countDate} countType={item.countType} onPressItem={() => onPressItem(item.id)}/>
+    <EventItem
+      title={item.title}
+      date={item.date}
+      countDate={item.countDate}
+      countType={item.countType}
+      onPressItem={() => onPressItem(item.id)}
+      prefixOffsetCount={item.prefixOffsetCount}
+    />
   );
 
   return (
@@ -96,11 +134,17 @@ const styles = StyleSheet.create({
   },
   itemRight: {
     padding: 10,
+    flexBasis: "40%",
+    borderRadius: 15,
+  },
+  itemRightTop: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  itemRightBottom: {
     alignItems: "center",
     justifyContent: "center",
-    flexBasis: "40%",
     flexDirection: "row",
-    borderRadius: 15,
   },
   title: {
     fontSize: 20,
