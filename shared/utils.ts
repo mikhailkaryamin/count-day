f52872153b3a9e7dec8ea715659a4ea14bb74e4c
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-13 21:17:17 +0300

    feat(utils): add calculateOffsetSelectedDays, fix calculateOffset

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,5 +1,5 @@
 import { MS, CountTypes } from "./consts";
-import { CountFor } from "./types";
+import { CountFor, CountOnlySelectedDays } from "./types";
 
 const calculateOffsetMS = (dateEvent: string) => {
   const currentDateMS = +(new Date());
@@ -10,25 +10,63 @@ const calculateOffsetMS = (dateEvent: string) => {
   return msOffset;
 };
 
-const calculateOffset = (dateEvent: string, type: CountFor): string => {
-  const UNDEFINED_COUNT = "undefined";
+const calculateOffset = (dateEvent: string, type: CountFor): number => {
+  const UNDEFINED_COUNT = 0;
   const ms = calculateOffsetMS(dateEvent);
+
+  const getRoundNumber = (numberMs: number) => {
+    return numberMs > 0 ? Math.floor(numberMs) : Math.ceil(numberMs);
+  };
 
   switch (type) {
     case CountTypes.Day:
-      return Math.floor(ms / MS.DAY).toString();
+      return getRoundNumber(ms / MS.DAY);
     case CountTypes.Week:
-      return Math.floor(ms / MS.WEEK).toString();
+      return getRoundNumber(ms / MS.WEEK);
     case CountTypes.Month:
-      return Math.floor(ms / MS.MONTH).toString();
+      return getRoundNumber(ms / MS.MONTH);
     case CountTypes.Year:
-      return Math.floor(ms / MS.YEAR).toString();
+      return getRoundNumber(ms / MS.YEAR);
     default:
       return UNDEFINED_COUNT;
   }
 };
 
+const calculateOffsetSelectedDays = (dateEvent: string, selectedDays: CountOnlySelectedDays): number => {
+  const DAYS_OF_WEEK = 6;
+  const DAYS_OF_TWO_WEEKS = 13;
+
+  const getCountSelectedDaysOfWeek = (selectedDaysValues: boolean[]) => {
+    return selectedDaysValues.reduce((acc, curVal) => {
+      return acc + +curVal;
+    }, 0);
+  };
+
+  const date = new Date(dateEvent);
+  const countDay = Math.abs(calculateOffset(dateEvent, CountTypes.Day));
+  const countWeek = Math.abs(calculateOffset(dateEvent, CountTypes.Week));
+
+  const selectedDaysOfWeek = selectedDays.map((dayItem) => dayItem[1]);
+  const currentDayOfWeek = new Date().getDay();
+  const lastDayFirstWeek = currentDayOfWeek + countDay;
+  const finishDayOfWeek = date.getDay() - 1;
+
+  const countSelectedDaysOnWeek = getCountSelectedDaysOfWeek(selectedDaysOfWeek);
+  const firstWeekSelectedDays = getCountSelectedDaysOfWeek(selectedDaysOfWeek.slice(currentDayOfWeek, lastDayFirstWeek));
+  const lastWeekSelectedDays = getCountSelectedDaysOfWeek(selectedDaysOfWeek.slice(0, finishDayOfWeek));
+
+  switch (true) {
+    case (countDay + firstWeekSelectedDays) <= DAYS_OF_WEEK:
+      return firstWeekSelectedDays;
+    case (countDay + firstWeekSelectedDays) <= DAYS_OF_TWO_WEEKS:
+      return firstWeekSelectedDays + lastWeekSelectedDays;
+    default:
+      return firstWeekSelectedDays + lastWeekSelectedDays + (countWeek * countSelectedDaysOnWeek);
+  }
+
+};
 
 export {
   calculateOffset,
+  calculateOffsetSelectedDays,
 };
