3c932848fe7ed6ba34e2950e4578fb82384dab68
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-14 16:10:17 +0300

    fix(utils): fix logic calculateOffsetSelectedDay and calculeteOffset

--- shared/utils.ts
+++ shared/utils.ts
@@ -14,28 +14,21 @@ const calculateOffset = (dateEvent: string, type: CountFor): number => {
   const UNDEFINED_COUNT = 0;
   const ms = calculateOffsetMS(dateEvent);
 
-  const getRoundNumber = (numberMs: number) => {
-    return numberMs > 0 ? Math.floor(numberMs) : Math.ceil(numberMs);
-  };
-
   switch (type) {
     case CountTypes.Day:
-      return getRoundNumber(ms / MS.DAY);
+      return Math.floor(ms / MS.DAY);
     case CountTypes.Week:
-      return getRoundNumber(ms / MS.WEEK);
+      return Math.floor(ms / MS.WEEK);
     case CountTypes.Month:
-      return getRoundNumber(ms / MS.MONTH);
+      return Math.floor(ms / MS.MONTH);
     case CountTypes.Year:
-      return getRoundNumber(ms / MS.YEAR);
+      return Math.floor(ms / MS.YEAR);
     default:
       return UNDEFINED_COUNT;
   }
 };
 
 const calculateOffsetSelectedDays = (dateEvent: string, selectedDays: CountOnlySelectedDays): number => {
-  const DAYS_OF_WEEK = 6;
-  const DAYS_OF_TWO_WEEKS = 13;
-
   const getCountSelectedDaysOfWeek = (selectedDaysValues: boolean[]) => {
     return selectedDaysValues.reduce((acc, curVal) => {
       return acc + +curVal;
@@ -45,25 +38,44 @@ const calculateOffsetSelectedDays = (dateEvent: string, selectedDays: CountOnlyS
   const date = new Date(dateEvent);
   const countDay = Math.abs(calculateOffset(dateEvent, CountTypes.Day));
   const countWeek = Math.abs(calculateOffset(dateEvent, CountTypes.Week));
+  const isDateBeforeCurrentDate = date < new Date();
 
   const selectedDaysOfWeek = selectedDays.map((dayItem) => dayItem[1]);
-  const currentDayOfWeek = new Date().getDay();
-  const lastDayFirstWeek = currentDayOfWeek + countDay;
-  const finishDayOfWeek = date.getDay() - 1;
+  const firstDayOfFirstWeek = isDateBeforeCurrentDate ? date.getDay() : new Date().getDay();
+  const lastDayFirstWeek = firstDayOfFirstWeek + countDay;
+  const finishDayOfLastWeek = isDateBeforeCurrentDate ? new Date().getDay() : date.getDay();
 
   const countSelectedDaysOnWeek = getCountSelectedDaysOfWeek(selectedDaysOfWeek);
-  const firstWeekSelectedDays = getCountSelectedDaysOfWeek(selectedDaysOfWeek.slice(currentDayOfWeek, lastDayFirstWeek));
-  const lastWeekSelectedDays = getCountSelectedDaysOfWeek(selectedDaysOfWeek.slice(0, finishDayOfWeek));
+  const firstWeekSelectedDays = getCountSelectedDaysOfWeek(
+      selectedDaysOfWeek.slice(firstDayOfFirstWeek, lastDayFirstWeek)
+  );
+  const lastWeekSelectedDays = getCountSelectedDaysOfWeek(
+      selectedDaysOfWeek.slice(0, finishDayOfLastWeek)
+  );
+
+  let offsetSelectedDays;
+
+  const DAYS_OF_WEEK = 6;
+  const DAYS_OF_TWO_WEEKS = 13;
+  const isOnlyOneWeek = (countDay + firstWeekSelectedDays) <= DAYS_OF_WEEK;
+  const isOnlyTwoWeeks = (countDay + firstWeekSelectedDays) <= DAYS_OF_TWO_WEEKS;
 
   switch (true) {
-    case (countDay + firstWeekSelectedDays) <= DAYS_OF_WEEK:
-      return firstWeekSelectedDays;
-    case (countDay + firstWeekSelectedDays) <= DAYS_OF_TWO_WEEKS:
-      return firstWeekSelectedDays + lastWeekSelectedDays;
+    case isOnlyOneWeek:
+      offsetSelectedDays = firstWeekSelectedDays;
+      break;
+    case isOnlyTwoWeeks:
+      offsetSelectedDays = firstWeekSelectedDays + lastWeekSelectedDays;
+      break;
     default:
-      return firstWeekSelectedDays + lastWeekSelectedDays + (countWeek * countSelectedDaysOnWeek);
+      offsetSelectedDays = firstWeekSelectedDays + lastWeekSelectedDays + (countWeek * countSelectedDaysOnWeek);
   }
 
+  if (!isDateBeforeCurrentDate) {
+    offsetSelectedDays *= -1;
+  }
+
+  return offsetSelectedDays;
 };
 
 export {
